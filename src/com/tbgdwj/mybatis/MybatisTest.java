d19abe00a6ff5968129d3a707cea12a412bbb44f
tbgdwj <17049185@qq.com>
2017-03-13 17:35:08 +0800

    add mybatis

---
+++ src/com/tbgdwj/mybatis/MybatisTest.java
@@ -0,0 +1,16 @@
+package com.tbgdwj.mybatis;
+
+public class MybatisTest {
+	/**
+	 * 什么是Mybatis
+	 *  MyBatis 本是apache的一个开源项目基于Java的持久层框架
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
