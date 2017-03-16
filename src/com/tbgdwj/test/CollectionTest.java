54249043ceb05fea555c4196538da30971fd4ce9
tbgdwj <17049185@qq.com>
2017-03-16 17:58:37 +0800

    1

--- src/com/tbgdwj/test/CollectionTest.java
+++ src/com/tbgdwj/test/CollectionTest.java
@@ -9,16 +9,16 @@ public class CollectionTest {
         List<String> list1 =new ArrayList<String>();
         List<String> list2 =new ArrayList<String>();
         
-        list1.add("abc1");
-        list1.add("abc2");
-        list1.add("abc3");
-        
-        list2.add("abc2");
-        list2.add("abc3");
-        list2.add("abc5");   
-        
-        System.out.println(list1);
-        System.out.println(list2);
+//        list1.add("abc1");
+//        list1.add("abc2");
+//        list1.add("abc3");
+//        
+//        list2.add("abc2");
+//        list2.add("abc3");
+//        list2.add("abc5");   
+//        
+//        System.out.println(list1);
+//        System.out.println(list2);
 //        System.out.println(list1.addAll(list2));
                
 //        list1.clear();
@@ -40,26 +40,53 @@ public class CollectionTest {
 //		 System.out.println(iterator.next());		
 //	   }
         
-       for (ListIterator<String> it = list2.listIterator(); it.hasNext();) {
-    	 Object str =it.next();
-    	 if (str.equals("abc3")){
-    	   it.add("abc4"); 
-    	 }
-   		 //System.out.println(iterator.next());		
-   	   }
-       System.out.println(list2); 
-       
-		// 获取列表迭代器
-		ListIterator<String> it=list2.listIterator();
-		
-		while(it.hasNext()){
-			Object obj=it.next();
-			if(obj.equals("abc2")){
-				it.add("hello");
+//       for (ListIterator<String> it = list2.listIterator(); it.hasNext();) {
+//    	 Object str =it.next();
+//    	 if (str.equals("abc3")){
+//    	   it.add("abc4"); 
+//    	 }
+//   		 //System.out.println(iterator.next());		
+//   	   }
+//       System.out.println(list2); 
+//       
+//		// 获取列表迭代器
+//		ListIterator<String> it=list2.listIterator();
+//		
+//		while(it.hasNext()){
+//			Object obj=it.next();
+//			if(obj.equals("abc2")){
+//				it.add("abc4");
+//			}
+//			//System.out.println(obj);
+//		}
+//		System.out.println(list2);  
+        
+        list2.add("abc2");
+        list2.add("abc3");
+        list2.add("abc2");         
+        list2.add("abc2");
+        list2.add("abc3");
+        list2.add("abc5"); 
+        list2.add("abc4");         
+        list2.add("abc2");        
+        System.out.println(list2);
+        
+		for(int i=0;i<list2.size()-1;i++){
+			for(int j=i+1;j<list2.size();j++){
+				if(list2.get(i).equals(list2.get(j))){
+					list2.remove(j);
+					j=j-1;
+				}
+				
 			}
-			//System.out.println(obj);
 		}
-		System.out.println(list2);      
+		System.out.println(list2); 
+
+        List<String> tmpList = new ArrayList<String>();
+        for (int i = 0; i < list2.size()-1; i++) {
+        	tmpList.contains(list2.get(i));
+		}
+        System.out.println(tmpList); 
        
 	}
 
